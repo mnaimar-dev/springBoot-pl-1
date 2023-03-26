9181c5eb32abea45f8881865e2809fd0e9f54b94
Nicolás Aimar <89144297+mnaimar-dev@users.noreply.github.com>
2023-03-26 02:04:36 -0300

    First Commit

---
+++ src/test/java/com/fundamentosPlatzi/springboot/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosPlatzi.springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
